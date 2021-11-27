523f7e0e1e2f26b3f2cdca662db3e63043b1e4a3
GBaykov <gleb.bykov99@gmail.com>
2021-11-27 20:09:59 +0300

    feat: implemet UUID serchin GET request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 const { v4: uid } = require('uuid');
 
 let db = [
@@ -10,21 +11,20 @@ let db = [
 ]
 
 const server = http.createServer((req, res) => {
-  
-  if (req.method === 'GET') {
-    res.writeHead(200, {
-      'Content-Type': 'application/json' 
-    })
-    if(req.url === '/person') {
-      res.end(JSON.stringify(db))
-    } 
 
-  } else if (req.method === 'POST') {
-    const body = []
-    res.writeHead(201, {
-      'Content-Type': 'application/json' //; charset=utf-8
-    })
-    if(req.url === '/person'){
+  if(req.url === '/person' || req.url === '/person/'){
+    if (req.method === 'GET') {
+       res.writeHead(200, {
+        'Content-Type': 'application/json' 
+        })   
+      res.end(JSON.stringify(db))
+
+    } else if (req.method === 'POST') {
+      const body = []
+      res.writeHead(201, {
+        'Content-Type': 'application/json' //; charset=utf-8
+      })
+
       req.on('data', data => {
         body.push(Buffer.from(data))
       })
@@ -33,25 +33,50 @@ const server = http.createServer((req, res) => {
         
         if(!newPerson.name || !newPerson.age || !newPerson.hobbies) {
           res.writeHead(404)
-          res.end('Error:incorrect properties')
+          res.write('Error:incorrect properties')
+          res.end()
         } else {
           newPerson.id = uid()
           db.push(newPerson)
           res.end(JSON.stringify(newPerson))
         }
       })
-    } 
-   
-    // req.on('end', () => {
-    //   const message = body.toString().split('=')[1]
-
-    //   res.end(`
-    //     <h1>Ваше сообщение: ${message}</h1>
-    //   `)
-    // })
+    }
+  }else{
+    const id = req.url.split('/')[2];
+    if (req.method === 'GET') {
+      res.writeHead(200, {
+        'Content-Type': 'application/json' 
+      })   
+      const person = idSearch(id, db);
+      if(person[0] === 404){
+        res.writeHead(404)
+        res.write('Error:cannot find person whis such ID')
+        res.end()
+      } else {
+        res.writeHead(200)
+        res.end(JSON.stringify(person[1]))
+      }
+    }
   }
 })
 
 server.listen(5000, () => {
   console.log('Server is running...')
 })
+
+
+function idSearch(id, arr){
+  let ob;
+  for(let obj of arr) {
+    if(obj.id == id){
+      ob = obj;
+      break;
+    } 
+  } 
+  if(typeof ob == 'object'){
+return [200, ob]
+  } else {
+    return [404]
+  }
+}
